5b3edc3055aa4c690e599504d5dea14cd4685e53
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2023-04-30 23:58:56 +0530

    Update User.java

--- src/main/java/com/example/UserManagementValidation/Model/User.java
+++ src/main/java/com/example/UserManagementValidation/Model/User.java
@@ -1,5 +1,9 @@
 package com.example.UserManagementValidation.Model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,14 +12,31 @@ import java.security.Timestamp;
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
     private String UserId;
+
+    @NotEmpty(message = "Not Suitable UserName")
+    @Pattern(regexp = "^[A-Za-z]\\w{5,29}$")
     private String UserName;
-    private String DOB;
-    private String Email;
-    private String Phone;
-    private LocalDate Date;
-    private Timestamp Time;
+
+    @NotEmpty(message = "Please make sure this DOB is correct")
+    @Pattern(regexp = "^(1[0-2]|0[1-9])-(3[01]" + "|[12][0-9]|0[1-9])-[0-9]{4}$")
+    private String dob;
+
+    @NotEmpty
+    @Email(message = "Email is not correct")
+    private String email;
+
+    @NotEmpty(message = "Phone Number is empty")
+    @Size(min = 10, max = 12)
+    //@Pattern(regexp = "^\\d{12}$") //Normal 12 digit phoneNumber validation regex
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{4,5}[- .]?\\d{5}$") //International PhoneNumber
+    //@Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$") //some Country PhoneNumber
+    private String phone;
+
+    private String date;
+    private String time;
 }
